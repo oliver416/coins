85d43cf511fb0a7dcd7150ba2144864ae06db7c7
user <user@mail.com>
2022-07-29 17:43:51 +0200

    024 CHG: assets admin page

--- apps/assets/admin.py
+++ apps/assets/admin.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.contrib import admin
 from django.utils.html import format_html
 
@@ -13,13 +15,27 @@ class AssetAdmin(admin.ModelAdmin):
     )
     list_display = (
         'id',
-        'name',
-        'amount',
+        'name_',
+        'amount_',
         'roi',
-        'current_price',
-        'price_usd',
+        'price',
+        'price_usd_',
         'wallet',
     )
+
+    @admin.display(description='Name')
+    def name_(self, asset: Asset):
+        ticker, name, symbol = str(asset.name).split(' - ')
+        html = f'''
+            <div><strong>{ticker}</strong></div>
+            <div>{name}</div>
+            <div>{symbol}</div>
+        '''
+        return format_html(html)
+
+    @admin.display(description='Amount')
+    def amount_(self, asset: Asset):
+        return asset.amount
 
     def roi(self, asset: Asset) -> str:
         roi = GetRoiService.get_roi(asset)
@@ -27,8 +43,14 @@ class AssetAdmin(admin.ModelAdmin):
         html = f'''<b style="color:{color}">{sign}{{}}%</b>'''
         return format_html(html, roi)
 
-    def current_price(self, asset: Asset) -> str:
-        return str(GetRoiService.get_asset_price(asset))
+    def price(self, asset: Asset) -> str:
+        price = GetRoiService.get_asset_price(asset)
+        return str(price.quantize(Decimal('1.00')))
+
+    @admin.display(description='Price USD')
+    def price_usd_(self, asset: Asset) -> str:
+        price = asset.price_usd
+        return str(price.quantize(Decimal('1.00')))
 
     def changelist_view(self, request, extra_context=None):
         CreateCurrencyRateService.fill_currency_rates()
